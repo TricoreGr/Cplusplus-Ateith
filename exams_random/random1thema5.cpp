ac60b45146170034294529b517fbf565fb9fbc12
Giorgos Apostolopoulos <s3apostol@gmail.com>
2019-06-25 17:06:58 +0300

    Update random1thema5.cpp

--- exams_random/random1thema5.cpp
+++ exams_random/random1thema5.cpp
@@ -1,73 +1,109 @@
 #include <iostream>
+
 using namespace std;
+
 class Box{
     public:
-        float m;
-        float p;
-        float y;
+        float m,p,y;
 };
-class Konteiner{
+
+
+
+class Container{
     private:
         Box *p;
         int n;
         float vol;
 
     public:
-        Konteiner(int n,float vol){
-            p=new Box[n];
-            this->n=n;
-            this->vol=vol;
-            for (int i=0; i<n;i++){
-                p[i].m=0;
-                p[i].p=0;
-                p[i].y=0;
+        Container(int n, float vol){
+            this -> n = n;
+
+            p = new Box[n];
+            for(int i=0; i<n; i++){
+                p[i].m = 0;
+                p[i].p = 0;
+                p[i].y = 0;
             }
+
+            this -> vol = vol;
         }
-        ~Konteiner(){
-            cout<<"The object is fucking destructed"<<endl;
+
+
+        Container(const Container& other){
+            cout << "Copy constructor called" << endl;
+            n = other.n;
+            vol = other.vol;
+            
+            p = new Box[n];
+            for (int i=0; i<n; i++)
+                p[i] = other.p[i];
         }
-        Konteiner(const Konteiner &obj){
-            this->p=obj.p;
-            this->n=obj.n;
-            this->vol=obj.vol;
-            cout<<"The copy constructor is called"<<endl;
+
+
+        ~Container(){
+            cout << "destroyeeeeeeeeeeeed" << endl;   
         }
-        void getAttributes(int& n,float& vol){
-            n=this->n;
-            vol=this->vol;
+
+
+        void getValues(Box*& p, int& n, float& vol){
+            n = this -> n;
+            vol = this -> vol;
+            p = this -> p;
         }
-        float getVolume(){
-            float sum=0;
-            for(int i=0;i<n;i++){
-                sum=sum+(p[i].y*p[i].p*p[i].m);
+
+
+        float freeVolume() {
+            float currentVolume = 0.0f;
+
+            for (int i=0; i<n; i++) 
+                currentVolume += (p[i].m * p[i].p * p[i].y);
+
+            return vol - currentVolume;
+        }
+
+        void addBox(Box& box) {
+            for (int i=0; i<n; i++){
+                if ( p[i].m==0 && p[i].p==0 && p[i].y==0 ){
+                    p[i].m = box.m;
+                    p[i].p = box.p;
+                    p[i].y = box.y;
+                    cout << "Box was added" << endl;
+                    return;
+                }
             }
-            return vol-sum;
+
+            cout << "No more space, box wasn't added" << endl;
         }
-        void addBox(Box& b){
-             for(int i=0;i<n;i++){
-                 if(p[i].m==0 && p[i].p==0 && p[i].y==0){
-                     p[i].m=b.m;
-                     p[i].p=b.p;
-                     p[i].y=b.y;
-                     return;
-                 }
-             }
-            cout<<"Box wasn't added"<<endl;
-        }
+
+
 };
-int main(){
-    Konteiner k(2,10);
-    Box box1{1,1,1};
-    Box box2{1,1,1};
-    Box box3{1,1,1};
+
+int main () {
+    Container c(2, 1000);
+
+    //add box test
+    Box b1{2,10,5.5f};
+    c.addBox(b1);
+
+    //copy constructor test
+    Container c2 = c;
+
+    //get values test
+    Box* p;
     int n;
     float vol;
-    k.getAttributes(n,vol);
-    cout<<"Attributes of container "<<n<<" and "<<vol<<endl;
-    cout<<"Volume "<<k.getVolume()<<endl;
-    k.addBox(box1);
-    k.addBox(box2);
-    k.addBox(box3);
-    cout<<"Volume "<<k.getVolume()<<endl;
-    Konteiner k2=k;
+    c2.getValues(p, n, vol);
+    cout << endl << "Boxes: " << endl;
+    for (int i=0; i<n; i++){
+        cout << "Box" << i+1 << " m: " << p[i].m << endl;
+        cout << "Box" << i+1 << " y: " << p[i].y << endl;
+        cout << "Box" << i+1 << " p: " << p[i].p << endl << endl;
+    }
+        
+    //available volume test
+    cout << "Available volume: " << c.freeVolume() << endl;
+
+    return 0;
+    
 }
